32c7b7a538e863a7a312a30965de66f491713711
LJM <1743390732@qq.com>
2025-06-12 10:48:29 +0800

    before Router

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -1,10 +1,24 @@
 <script setup>
-import { defineProps } from 'vue';
-defineProps({
+import { defineProps, ref, computed } from 'vue';
+const props = defineProps({
   job: {
     type: Object,
     required: true,
   },
+});
+
+const showFullDescription = ref(false);
+
+const toggleFullDescription = () => {
+  showFullDescription.value = !showFullDescription.value;
+};
+
+const truncatedDescription = computed(() => {
+  let description = props.job.description;
+  if (!showFullDescription.value) {
+    description = description.substring(0, 90) + '...';
+  }
+  return description;
 });
 </script>
 
@@ -17,7 +31,15 @@ defineProps({
       </div>
 
       <div class="mb-5">
-        {{ job.description }}
+        <div>
+          {{ truncatedDescription }}
+        </div>
+        <button
+          @click="toggleFullDescription"
+          class="text-green-500 hover:text-green-600 mb-5"
+        >
+          {{ showFullDescription ? 'Less' : 'More' }}
+        </button>
       </div>
 
       <h3 class="text-green-500 mb-2">{{ job.salary }} / Year</h3>
@@ -26,7 +48,7 @@ defineProps({
 
       <div class="flex flex-col lg:flex-row justify-between mb-4">
         <div class="text-orange-700 mb-3">
-          <i class="fa-solid fa-location-dot text-lg"></i>
+          <i class="pi pi-map-marker text-orange-700"></i>
           {{ job.location }}
         </div>
         <a
