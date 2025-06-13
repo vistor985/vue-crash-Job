eaab57af8f8938e0b34438063f556361841dd4ad
LJM <1743390732@qq.com>
2025-06-13 11:03:00 +0800

    removeuselessdata

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@ import JobListings from '@/components/JobListings.vue';
 </script>
 
 <template>
-  <Hero title="Test Title" subtitle="Test subtitle" />
+  <Hero />
   <HomeCards />
   <JobListings :limit="3" :showButton="true" />
 </template>
